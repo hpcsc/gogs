9f891f5bdb8f22c84341774f8c082e6e860478b1
David Nguyen <hpcsc@users.noreply.github.com>
2019-10-09 22:47:42 +1100

    allow creating multiple repositories from script

--- scripts/create-git-repo.sh
+++ scripts/create-git-repo.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-REPO_NAME=$1
-
-if [[ -z "${REPO_NAME}" ]]; then
+if [[ "$#" -lt 1 ]]; then
     echo "=== Repo name is required"
-    echo "=== Usage: $0 repo-name"
+    echo "=== Usage: $0 repo-name-1 repo-name-2"
     exit 1
 fi;
 
@@ -14,11 +12,28 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 source ${SCRIPT_DIR}/_read_user_credentials.sh
 
+echo
+
 TOKEN=$(source ${SCRIPT_DIR}/generate-token.sh "" | tr -d "\n")
 
-curl -H 'Accept:application/json' \
-     -H 'Content-Type:application/json' \
-     -H "Authorization: token ${TOKEN}" \
-     -d '{"name":"'${REPO_NAME}'"}' \
-     -X POST \
-     http://localhost:3000/api/v1/user/repos
+for REPO_NAME in "$@"
+do
+    EXISTING_REPO=$(curl -H "Authorization: token ${TOKEN}" \
+                         -s \
+                         http://localhost:3000/api/v1/user/repos | \
+                         jq '.[] | select(.name == "'${REPO_NAME}'")')
+
+    if [[ -z ${EXISTING_REPO} ]]; then
+        curl -H 'Accept:application/json' \
+            -H 'Content-Type:application/json' \
+            -H "Authorization: token ${TOKEN}" \
+            -d '{"name":"'${REPO_NAME}'"}' \
+            -X POST \
+            http://localhost:3000/api/v1/user/repos
+
+        echo
+        echo "== ${REPO_NAME} created"
+    else
+        echo "=== ${REPO_NAME} exists"
+    fi
+done
